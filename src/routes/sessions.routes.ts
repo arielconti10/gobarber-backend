18f3cf12742eb189abc93030e520660a829eed0b
Ariel Conti <ariel@edoxa.gg>
2020-11-24 12:33:47 -0300

    Exception Handling

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,7 +5,6 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
     const { email, password } = request.body;
 
     const authenticateUser = new AuthenticateUserService();
@@ -24,10 +23,6 @@ sessionsRouter.post('/', async (request, response) => {
       userWithoutPassword,
       token
     })
-
-  } catch (err) {
-    return response.status(400).json({error: err.message})
-  }
 })
 
 export default sessionsRouter;
