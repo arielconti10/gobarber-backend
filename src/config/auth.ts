bda838389f595a72ed8ed006b174f710713ceb05
Ariel Conti <ariel@edoxa.gg>
2020-11-24 10:19:18 -0300

    first commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '869aa37dce6b803ad604ecb5d154de71',
+    expiresIn: '1d'
+  }
+}
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import './database';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333');
+});
